583dfcb244e3fbcfef89fe21b66b82cd6a16dfce
alanyoung <epubor_yl@163.com>
2020-11-10 19:20:23 +0800

    add practicedemo

---
+++ PracticeDemo/PracticeDemo/Entities/Author.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeDemo.Entities
+{
+    
+    
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+        
+        [Required, MaxLength(20)]
+        public string Name { get; set; }
+        
+        [Required]
+        public DateTimeOffset BirthDate { get; set; }
+        
+        [Required, MaxLength(40)]
+        public string BirthPlace { get; set; }
+
+        [Required,EmailAddress]
+        public string Email { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+
+
+}
---
+++ PracticeDemo/PracticeDemo/Entities/LibraryDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeDemo.Entities
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //添加种子数据，使用扩展方法的方式
+            modelBuilder.SeedData();
+
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+
+
+
+}
